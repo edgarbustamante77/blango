7060210fcc500f9788066fe0e0ee791c140a2fd5
Edgar Verdugo <e.bustamante@isteps.edu.ec>
2025-03-18 21:54:07 +0000

    Finish pagination and django filter

--- blango/settings.py
+++ blango/settings.py
@@ -85,7 +85,8 @@ class Dev(Configuration):
       'django.contrib.staticfiles',
       'blog',
       'blango_auth',
-      'drf_yasg'
+      'drf_yasg',
+      'django_filters'
   ]
 
   MIDDLEWARE = [
@@ -201,6 +202,12 @@ class Dev(Configuration):
         "user_sustained": "5000/day",
         "user_burst": "100/minute",
     },
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 2,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter"
+    ],
   }
 
   SWAGGER_SETTINGS = {
